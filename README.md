6d8194fc34b1a446c404a202720b2b48cc7ab06e
Florian Labarre <contact@florianlabarre.fr>
2024-01-07 00:30:59 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,119 @@
+<!-- Improved compatibility of back to top link: See: https://github.com/othneildrew/Best-README-Template/pull/73 -->
+<a name="readme-top"></a>
+
+
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+
+  <h3 align="center">Remarkable to Obsidian Sync</h3>
+
+  <p align="center">
+    This project syncs but most important converts your remarkable text files to markdown files .
+    <br />
+    <br />
+    <a href="https://github.com/flolbr/remarkable-obsidian-sync/issues">Report Bug</a>
+    <a href="https://github.com/flolbr/remarkable-obsidian-sync/issues">Request Feature</a>
+  </p>
+</div>
+
+<!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#prerequisites">Prerequisites</a></li>
+        <li><a href="#installation">Installation</a></li>
+      </ul>
+    </li>
+    <li><a href="#usage">Usage</a></li>
+    <li><a href="#contributing">Contributing</a></li>
+    <li><a href="#license">License</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#acknowledgments">Acknowledgments</a></li>
+  </ol>
+</details>
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+The usage is pretty straight forward. There are some dependencies, handled by poetry.
+
+
+### Prerequisites
+
+This is using poetry, you need it.
+
+### Installation
+
+1. Make sure you have something to copy the files from the remarkable to your machine. I use Rsync which comes with MacOs and almost every Linux distribution.
+
+2. Clone the repo
+   ```sh
+   git clone git@github.com:flolbr/remarkable-obsidian-sync.git
+   ```
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- USAGE EXAMPLES -->
+## Usage
+
+Sync your remarkables with RSYNC (or any other tool). This works the best if you have set-up passwordless login with the help of SSH keys. The best explenation how to do this I found this [Reddit Post](https://www.reddit.com/r/RemarkableTablet/comments/78u90n/passwordless_ssh_setup_for_remarkable_tablet/).
+
+TODO
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- CONTRIBUTING -->
+## Contributing
+
+Contributions are what make the open source community such an amazing place to learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+If you have a suggestion that would make this better, please fork the repo and create a pull request. You can also simply open an issue with the tag "enhancement".
+Don't forget to give the project a star! Thanks again!
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- LICENSE -->
+## License
+
+Distributed under the MIT License. See `LICENSE.txt` for more information.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- CONTACT -->
+## Contact
+You can reach me on Github!
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- ACKNOWLEDGMENTS -->
+## Acknowledgments
+
+* Inspired from: [https://github.com/EelcovanVeldhuizen/remarkable-obsidian-sync](https://github.com/EelcovanVeldhuizen/remarkable-obsidian-sync)
+* [RMScene by Ricklupton](https://github.com/ricklupton/rmscene)
+* [RMC by Ricklupton](https://github.com/ricklupton/rmc)
+* [Best README Template by Othneildrew](https://github.com/othneildrew/Best-README-Template)
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
---
+++ main.py
@@ -0,0 +1,271 @@
+import json
+import os
+import re
+from pathlib import Path
+from typing import Optional
+
+from dotenv import load_dotenv
+from rmscene import read_tree
+from rmscene.scene_items import ParagraphStyle
+from rmscene.text import TextDocument
+
+load_dotenv()
+
+IMPORT_TAG = 'Obsidian/Import'
+REMARKABLE_DIRECTORY = os.getenv('REMARKABLE_DIRECTORY', 'app/remarkables')
+VAULT_DIRECTORY = os.getenv('VAULT_DIRECTORY', 'app/vault')
+INGEST_DIRECTORY = os.getenv('INGEST_DIRECTORY', '99 - Ingest')
+TEMPLATE_DIRECTORY = os.getenv('TEMPLATE_DIRECTORY', '91 - Templates')
+
+template_directory = Path(f"{VAULT_DIRECTORY}/{TEMPLATE_DIRECTORY}")
+available_templates = set(x.stem for x in template_directory.glob('*.md'))
+
+
+def get_uuids_to_process(directory: Path, filter_tag: Optional[str] = IMPORT_TAG) -> list:
+    """
+    Get a list of uuids to process from a directory.
+    """
+    # Get a list of directories
+    directories = filter(lambda d: not d.name.endswith(".thumbnails"), [x for x in directory.iterdir() if x.is_dir()])
+    uuids = [x.name for x in directories]
+    # Filter out any possible invalid files / directories that are not a uuid
+    uuids = list(filter(
+        lambda d: re.search(r'^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$',
+                            d), uuids))  # noqa: E501
+
+    if filter_tag is None:
+        return uuids
+
+    output = []
+    for uuid in uuids:
+        content = RM_read_json(directory / f"{uuid}.content")
+        try:
+            tags = [tag['name'] for tag in content['tags']]
+        except TypeError:
+            tags = []
+        try:
+            page_tags = [tag['name'] for tag in content['pageTags']]
+        except TypeError:
+            page_tags = []
+        print(tags, page_tags)
+        if filter_tag in tags + page_tags:
+            output.append(uuid)
+
+    return output
+
+
+def RM_read_json(filepath: Path) -> Optional[dict]:
+    if os.path.isfile(filepath):
+        data = open(filepath, 'r', encoding='utf8').read().strip()
+        return None if data == "Blank" else json.loads(data)
+    return None
+
+
+def extract_doc(path):
+    with open(path, "rb") as f:
+        tree = read_tree(f)
+        assert tree.root_text
+        doc = TextDocument.from_scene_item(tree.root_text)
+        return doc
+
+
+class RemarkablePage:
+    def __init__(self, path: Path, parent: 'RemarkableDocument'):
+        self.path = path
+        self.uuid = path.stem
+        self.parent = parent
+        self._text_document = extract_doc(self.path)
+
+        self.tags = set(tag['name'] for tag in self.parent.content['pageTags'] if tag['pageId'] == self.uuid)
+
+        self.page_number = next((i + 1 for i, page in enumerate(self.parent.content['cPages']['pages']) if page['id'] == self.uuid), 0)
+        self.name = f"{self.parent.name} - Page {self.page_number}"
+
+        # Improve the name of the page
+        try:
+            if self._text_document.contents[0].style.value == ParagraphStyle.HEADING:
+                self.name = f"{self.parent.name} - {self._text_document.contents[0].contents[0].s}"
+        except IndexError:
+            pass
+
+    def to_obsidian(self, template: Optional[str] = None) -> Optional[str]:
+        text = ""
+        if len(self._text_document.contents) == 0:
+            return None
+
+        old_style = ParagraphStyle.PLAIN
+        for paragraph in self._text_document.contents:
+            # Insert a line break if the paragraph is the beginning of a list
+            if old_style == ParagraphStyle.PLAIN and \
+                    ParagraphStyle.BULLET <= paragraph.style.value <= ParagraphStyle.CB_CHECKED:
+                text += '\n'
+
+            # Insert the correct markdown for the paragraph style
+            match paragraph.style.value:
+                case ParagraphStyle.HEADING:
+                    text += '\n## '
+                case ParagraphStyle.PLAIN:
+                    pass
+                case ParagraphStyle.BOLD:
+                    text += "\n### "
+                case ParagraphStyle.BULLET:
+                    text += "- "
+                case ParagraphStyle.BULLET2:
+                    text += "    - "
+                case ParagraphStyle.CB_EMPTY:
+                    text += "- [ ] "
+                case ParagraphStyle.CB_CHECKED:
+                    text += "- [x] "
+                # case ParagraphStyle.BASIC: # Never seen in RM documents, could be useful at some point ?
+                #     pass
+                case _:
+                    raise NotImplementedError(f"Paragraph style {paragraph.style} not implemented")
+
+            # Insert the text, wrap with styling
+            for content in paragraph.contents:
+                before = ('**' * (content.properties['font-weight'] == 'bold') +
+                          '_' * (content.properties['font-style'] == 'italic'))
+                after = ('_' * (content.properties['font-style'] == 'italic') +
+                         '**' * (content.properties['font-weight'] == 'bold'))
+                text += before + content.s + after
+
+            # End of line breaks
+            text += '\n'
+            if paragraph.style.value in [ParagraphStyle.HEADING, ParagraphStyle.BOLD]:
+                text += '\n'
+
+            old_style = paragraph.style.value
+
+        if template:
+            template_text = open(template_directory / f"{template}.md", 'r', encoding='utf8').read()
+            text = template_text.replace('<% tp.file.selection() %>', text)
+
+        return text.strip()
+
+    def replace_tag(self, frm, to):
+        for tag in self.parent.content['pageTags']:
+            if tag['pageId'] == self.uuid and tag['name'] == frm:
+                tag['name'] = to
+                self.parent.save_content()
+                return
+
+    def __str__(self):
+        return f"RemarkablePage(uuid={self.uuid})"
+
+    def __repr__(self):
+        return f"RemarkablePage(uuid={self.uuid})"
+
+
+class RemarkableDocument:
+    def _find_pages(self) -> list[Path]:
+        # return list(self._root_directory.glob(f"{self._uuid}/*.rm"))
+        return [self._path / f"{page['id']}.rm" for page in self.content['cPages']['pages']]
+
+    def _read_json(self, filename: str) -> dict:
+        if os.path.isfile(self._root_directory / filename):
+            data = open(self._root_directory / filename, 'r').read().strip()
+            return {} if data == "Blank" else json.loads(data)
+        return {}
+
+    def __init__(self, uuid: str, root_directory: Path):
+        self._root_directory = root_directory
+        self._uuid = uuid
+        self._path = self._root_directory / self._uuid
+
+        self.content = self._read_json(f'{uuid}.content')  # TODO: convert to RemarkableContent
+        self.metadata = self._read_json(f'{uuid}.metadata')  # TODO: convert to RemarkableMetadata
+
+        self.tags = [tag['name'] for tag in self.content['tags']]
+
+        self.name = self.metadata.get('visibleName', 'Untitled')
+        self.background = "pdf" in self.content['fileType']
+
+        self.pages = [RemarkablePage(p, self) for p in self._find_pages()]
+
+        # TODO: add an array of parent directories
+
+    def page_by_id(self, uuid: str) -> RemarkablePage:
+        return next(filter(lambda p: p.uuid == uuid, self.pages))
+
+    def to_obsidian(self, template: str = None) -> str:
+        text = f"# {self.name}\n\n"
+        for page in self.pages:
+            text += page.to_obsidian() + '\n'
+
+        if template:
+            template_text = open(template_directory / f"{template}.md", 'r', encoding='utf8').read()
+            text = template_text.replace('<% tp.file.selection() %>', text)
+
+        return text
+
+    def replace_tag(self, frm, to):
+        for tag in self.content['tags']:
+            if tag['name'] == frm:
+                tag['name'] = to
+                self.save_content()
+                return
+
+    def save_content(self):
+        with open(self._root_directory / f"{self._uuid}.content", "w", encoding='utf8') as f:
+            json.dump(self.content, f, indent=4)
+
+    def __str__(self):
+        return f"RemarkableDocument(name={self.name}, uuid={self._uuid})"
+
+    def __repr__(self):
+        return f"RemarkableDocument(name={self.name}, uuid={self._uuid})"
+
+
+def app(remarkables_directory: Path, vault_directory, overwrite: bool = True) -> bool:
+    def save(name: str, content: str) -> None:
+        directory = vault_directory / INGEST_DIRECTORY
+        directory.mkdir(parents=True, exist_ok=True)
+        file_path = directory / f"{name}.md"
+        if file_path.exists() and not overwrite:
+            print(f"File {file_path} already exists, skipping")
+            return False
+
+        with open(file_path, "w", encoding='utf8') as f:
+            f.write(content)
+
+        return True
+
+    uuids = get_uuids_to_process(remarkables_directory)
+
+    print(uuids)
+
+    rm_docs = [RemarkableDocument(uuid, remarkables_directory) for uuid in uuids]
+
+    for rm_doc in rm_docs:
+        choice = input(f'Do you want to process "{rm_doc.name}" ? ([y]/n)')
+        # choice = True
+        if choice == 'n':
+            continue
+        if IMPORT_TAG in rm_doc.tags:
+            print(f"Processing {rm_doc.name}")
+            obsidian = rm_doc.to_obsidian()
+            print(obsidian)
+            if save(rm_doc.name, obsidian):
+                rm_doc.replace_tag(IMPORT_TAG, 'Obsidian/Imported')
+        else:
+            for page in rm_doc.pages:
+                if IMPORT_TAG in page.tags:
+                    print(f"Processing {page}")
+                    template_tags = list(page.tags & available_templates)
+                    if len(template_tags) == 1:
+                        obsidian = page.to_obsidian(template_tags[0])
+                    else:
+                        if len(template_tags) > 1:
+                            print(f"Multiple templates found for {page}, not using any template")
+                        obsidian = page.to_obsidian()
+                    print(obsidian)
+                    if save(page.name, obsidian):
+                        page.replace_tag(IMPORT_TAG, 'Obsidian/Imported')
+
+    # TODO: update and save the tags
+
+    exit(0)
+
+
+if __name__ == "__main__":
+    app(Path(REMARKABLE_DIRECTORY), Path(VAULT_DIRECTORY))
